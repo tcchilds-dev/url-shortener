1a4a401edd4218932cc1dc486d80fb04c180593c
Thomas Childs <thomas.c.childs@gmail.com>
2025-12-04 19:38:13 +0000

    refactor(docker+env): improve security on docker compose file

--- src/utils/client.ts
+++ src/utils/client.ts
@@ -1,6 +1,17 @@
 import { drizzle } from "drizzle-orm/node-postgres";
 import "dotenv/config";
 
-const db = drizzle(process.env.DATABASE_URL!);
+const NODE_ENV = process.env.NODE_ENV;
+
+const DB_URL =
+  NODE_ENV === "test" && process.env.TEST_DATABASE_URL
+    ? process.env.TEST_DATABASE_URL
+    : process.env.DATABASE_URL;
+
+if (!DB_URL) {
+  throw new Error("FATAL: DATABASE_URL environment variable is not set.");
+}
+
+const db = drizzle(DB_URL);
 
 export default db;
